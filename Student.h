dce4916cc203fb83062f7a92727bb3b8b4d9a4b9
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-15 18:07:45 -0700

    Completed Student struct

--- Student.h
+++ Student.h
@@ -11,5 +11,9 @@ using namespace std;
 // Student structure
 struct Student
 {
-    
+    string name;        // The student's name
+    int studentID;      // The student's id
+    int age;            // The student's age 
+    string address;     // The student's current address 
+    string* courses;    // All the courses that the student is currently taking
 };
