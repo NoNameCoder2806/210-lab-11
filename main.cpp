fb863a8d449a4b407bb56b87183f5e101681bf5a
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-15 18:17:19 -0700

    Setting up the main() function

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// COMSC-210 | Lab 11 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+// Libraries and header files
+#include <iostream>
+#include <string>
+#include "Student.h"
+using namespace std;
+
+// Function prototypes
+void displayStudent (const Student& student);
+
+int main ()
+{
+    return 0;
+}
+
+// Function implementations
+/*
+    displayStudent()
+    Display all the information of a Student struct.
+    Arguments: a Student struct
+    Returns: none
+*/
+void displayStudent (const Student& student)
+{
+
+}
