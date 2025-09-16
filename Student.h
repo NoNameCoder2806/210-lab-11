10a3e593131cb60f4f2a8b11addf498d4ee71c9a
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-15 17:58:18 -0700

    Initialize Lab 11, Creating Student Struct

---
+++ Student.h
@@ -0,0 +1,15 @@
+// COMSC-210 | Lab 11 | Dat Hoang Vien
+// IDE used: Visual Code Studio
+
+#pragma once
+
+// Libraries
+#include <iostream>
+#include <string>;
+using namespace std;
+
+// Student structure
+struct Student
+{
+    
+};
