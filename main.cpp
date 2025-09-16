eb4845b114561533927c05843054bfb3b9e315ae
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-15 18:28:41 -0700

    Setting up the functions in the main file

--- main.cpp
+++ main.cpp
@@ -7,22 +7,42 @@
 #include "Student.h"
 using namespace std;
 
+// Constants
+const int ARRAY_SIZE = 100;
+
 // Function prototypes
-void displayStudent (const Student& student);
+void addStudent(Student* studentArr, int& count);            // Add a new Student to the array
+
+void displayStudent(const Student& student);     // Display the information of a Student object
 
 int main ()
 {
+    // Dynamically create an array of Student type objects
+    Student* studentArr = nullptr;
+    studentArr = new Student[ARRAY_SIZE];
+
     return 0;
 }
 
 // Function implementations
+/*
+    addStudent()
+    Add a new Student object to the existing array
+    Arguments: a Student array pointer, a count integer to keep track of the array size
+    Returns: none
+*/
+void addStudent(Student* studentArr, int& count)
+{
+
+}
+
 /*
     displayStudent()
     Display all the information of a Student struct.
     Arguments: a Student struct
     Returns: none
 */
-void displayStudent (const Student& student)
+void displayStudent(const Student& student)
 {
 
 }
